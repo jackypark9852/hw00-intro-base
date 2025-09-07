87b560155e1d2f9424bdef6bbeee8c8aebbcc62c
Jacky Park <jackypark9852@gmail.com>
2025-09-07 19:32:28 -0400

    feat: add color controls

--- src/rendering/gl/OpenGLRenderer.ts
+++ src/rendering/gl/OpenGLRenderer.ts
@@ -22,16 +22,20 @@ class OpenGLRenderer {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   }
 
-  render(camera: Camera, prog: ShaderProgram, drawables: Array<Drawable>) {
+  render(camera: Camera, prog: ShaderProgram, drawables: Array<Drawable>, color: number[]) {
     let model = mat4.create();
     let viewProj = mat4.create();
-    let color = vec4.fromValues(1, 0, 0, 1);
 
     mat4.identity(model);
     mat4.multiply(viewProj, camera.projectionMatrix, camera.viewMatrix);
     prog.setModelMatrix(model);
     prog.setViewProjMatrix(viewProj);
-    prog.setGeometryColor(color);
+    prog.setGeometryColor(vec4.fromValues(
+      color[0] / 255.0,
+      color[1] / 255.0,
+      color[2] / 255.0, 
+      1.0
+    ));
 
     for (let drawable of drawables) {
       prog.draw(drawable);
